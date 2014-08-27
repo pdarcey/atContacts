95bcce0190376167304d7590b59f90907a214d0e
Paul Darcey <pdarcey@gmail.com>
2014-08-27 09:57:56 +0100

    Initial Commit

---
+++ atContacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  atContacts
+//
+//  Created by Paul Darcey on 27/08/2014.
+//  Copyright (c) 2014 Paul Darcey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ atContacts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  atContacts
+//
+//  Created by Paul Darcey on 27/08/2014.
+//  Copyright (c) 2014 Paul Darcey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
