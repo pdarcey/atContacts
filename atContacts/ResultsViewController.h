ce70b79b0592ea4dae2628a65854598dd66dee64
Paul Darcey <pdarcey@gmail.com>
2014-09-01 17:44:20 +0100

    Added Results view controller

---
+++ atContacts/ResultsViewController.h
@@ -0,0 +1,24 @@
+//
+//  ResultsViewController.h
+//  atContacts
+//
+//  Created by Paul Darcey on 1/09/2014.
+//  Copyright (c) 2014 Paul Darcey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *firstName;
+@property (weak, nonatomic) IBOutlet UILabel *lastName;
+@property (weak, nonatomic) IBOutlet UIImageView *photo;
+@property (weak, nonatomic) IBOutlet UILabel *twitterHandle;
+@property (weak, nonatomic) IBOutlet UILabel *hashtag;
+@property (weak, nonatomic) IBOutlet UITextField *email;
+@property (weak, nonatomic) IBOutlet UITextField *phone;
+@property (weak, nonatomic) IBOutlet UITextField *webAddress;
+@property (weak, nonatomic) IBOutlet UITextView *twitterDescription;
+@property (weak, nonatomic) IBOutlet UIImageView *indicator;
+
+@end
---
+++ atContacts/ResultsViewController.m
@@ -0,0 +1,37 @@
+//
+//  ResultsViewController.m
+//  atContacts
+//
+//  Created by Paul Darcey on 1/09/2014.
+//  Copyright (c) 2014 Paul Darcey. All rights reserved.
+//
+
+#import "ResultsViewController.h"
+
+@interface ResultsViewController ()
+
+@end
+
+@implementation ResultsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
