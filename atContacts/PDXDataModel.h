3432accb3bbef0d64ecce751092e4144dbcc224a
Paul Darcey <pdarcey@gmail.com>
2014-09-02 11:40:41 +0100

    Added DataModel

---
+++ atContacts/PDXDataModel.h
@@ -0,0 +1,22 @@
+//
+//  PDXDataModel.h
+//  atContacts
+//
+//  Created by Paul Darcey on 2/09/2014.
+//  Copyright (c) 2014 Paul Darcey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PDXDataModel : NSObject
+
+@property (strong, nonatomic) NSString *firstName;
+@property (strong, nonatomic) NSString *lastName;
+@property (strong, nonatomic) NSString *twitterName;
+@property (strong, nonatomic) NSString *hashtag;
+@property (strong, nonatomic) NSString *emailAddress;
+@property (strong, nonatomic) NSString *phoneNumber;
+@property (strong, nonatomic) NSString *wwwAddress;
+@property (strong, nonatomic) NSString *twitterDescription;
+
+@end
---
+++ atContacts/PDXDataModel.m
@@ -0,0 +1,13 @@
+//
+//  PDXDataModel.m
+//  atContacts
+//
+//  Created by Paul Darcey on 2/09/2014.
+//  Copyright (c) 2014 Paul Darcey. All rights reserved.
+//
+
+#import "PDXDataModel.h"
+
+@implementation PDXDataModel
+
+@end
