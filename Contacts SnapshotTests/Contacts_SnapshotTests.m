abea667983621f0fb6a43fba3c90658355a26bf7
Paul Darcey <pdarcey@gmail.com>
2014-11-18 14:05:47 +1100

    Separated out the snapshot tests into their own target, and excluded them from the shared scheme used by the CI server

---
+++ Contacts SnapshotTests/Contacts_SnapshotTests.m
@@ -0,0 +1,28 @@
+//
+//  Contacts_SnapshotTests.m
+//  Contacts SnapshotTests
+//
+//  Created by Paul Darcey on 18/11/2014.
+//  Copyright (c) 2014 Paul Darcey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Contacts_SnapshotTests : XCTestCase
+
+@end
+
+@implementation Contacts_SnapshotTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
