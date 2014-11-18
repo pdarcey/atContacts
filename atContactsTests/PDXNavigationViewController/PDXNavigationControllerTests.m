9bece176d0ac55bbe6e2d6d090fb4dbc10b020ee
Paul Darcey <pdarcey@gmail.com>
2014-11-18 13:28:10 +1100

    Changed PDXNavigationController record mode to YES, so that CI server can set up reference images

--- atContactsTests/PDXNavigationViewController/PDXNavigationControllerTests.m
+++ atContactsTests/PDXNavigationViewController/PDXNavigationControllerTests.m
@@ -23,7 +23,7 @@
     // Flip this to YES to record images in the reference image directory.
     // You need to do this the first time you create a test and whenever you change the snapshotted views.
     // Tests running in record mode will allways fail so that you know that you have to do something here before you commit.
-    self.recordMode = NO;
+    self.recordMode = YES;
 }
 
 - (void)tearDown {
